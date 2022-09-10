52ee089a8ae4e554c0f6ad09508caf9343f1efdf
antykom <36765862+antykom@users.noreply.github.com>
2022-09-10 11:48:19 +0200

    22.2 Rest Template module updated

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -3,17 +3,17 @@ package com.crud.tasks.controller;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("v1/trello")
 @RequiredArgsConstructor
-@CrossOrigin("*")
 public class TrelloController {
 
     private final TrelloClient trelloClient;
@@ -22,10 +22,17 @@ public class TrelloController {
     public void getTrelloBoards() {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> {
-            if (!trelloBoardDto.getId().isEmpty() && !trelloBoardDto.getName().isEmpty() && trelloBoardDto.getName().contains("Kodilla")) {
-                System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-            }
-        });
+        trelloBoards.stream()
+                .filter(e -> !e.getId().isEmpty())
+                .filter(e -> !e.getName().isEmpty() && e.getName().contains("Kodilla"))
+                .peek(e -> System.out.println(e.getId() + " " + e.getName()))
+                .collect(Collectors.toList());
+/**
+ trelloBoards.forEach(trelloBoardDto -> {
+ if (!trelloBoardDto.getId().isEmpty() && !trelloBoardDto.getName().isEmpty() && trelloBoardDto.getName().contains("Kodilla")) {
+ System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+ }
+ });
+ **/
     }
 }
